1f3b158e970fea5642803056a9f3061385e589b4
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-03-18 20:05:47 +0300

    creation of data to be seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,70 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create([
+    {
+        name:"Pizza Palace",
+        address:"123 Githurai"
+    },
+    {
+        name:"Gallitos",
+        address:"456 Nairobi"
+    },
+    {
+        name:"Pizza Inn",
+        address:"456 Dandora"
+    }
+])
+
+Pizza.create([
+    {
+        name:"Veggie",
+        ingredients:"Carrot and Onions",
+    },
+    {
+        name:"Pepperoni",
+        ingredients:"Nyanya and hoho",
+
+    },
+    {
+        name:"Hawaiian",
+        ingredients:"Pinneaple and Onions",
+
+    },
+    {
+        name:"Berbeque",
+        ingredients:"Goat meat",
+
+    }
+])
+
+RestaurantPizza.create([
+    {
+        pizza_id:1,
+        restaurant_id: 1
+    },
+    {
+        pizza_id:1,
+        restaurant_id: 2
+    },
+    {
+        pizza_id:2,
+        restaurant_id:2
+    },
+    {
+        pizza_id:2,
+        restaurant_id:3
+    },
+    {
+        pizza_id:2,
+        restaurant_id:4
+    },
+    {
+        pizza_id:3,
+        restaurant_id:1
+    },
+    {
+        pizza_id:3,
+        restaurant_id:3
+    }
+])
