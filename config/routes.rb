bae1114979b8906cf393a3fd2b80701c4a36abb9
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-03-18 20:04:21 +0300

    creation of resources and CRUD operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :restaurants, only: [:index,:show,:destroy]
+  resources :restaurant_pizzas, only: [:index,:create]
+  resources :pizzas, only: [:index,:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
